bc84772ff5d68338bfb33118c5098967092a8927
lfdyf20 <lfdyf20@163.com>
2017-09-25 15:59:22 -0700

    add detail storyboard

--- DreamLister/Controller/MainVC.swift
+++ DreamLister/Controller/MainVC.swift
@@ -24,19 +24,48 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         // Do any additional setup after loading the view, typically from a nib.
         tableView.delegate = self
         tableView.dataSource = self
+
+//        generateTestData()
+        attemptFetch()
     }
 
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
+        configureCell(cell: cell, indexPath: indexPath)
+        return cell
+    }
+
+    func configureCell(cell: ItemCell, indexPath: IndexPath) {
+
+        //update the cell
+
+        let item = controller.object(at: indexPath as IndexPath)
+        cell.configureCell(item: item)
+
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let sections = controller.sections {
+
+            let sectionInfo = sections[section]
+            return sectionInfo.numberOfObjects
+        }
+
         return 0
     }
 
     func numberOfSections(in tableView: UITableView) -> Int {
+        if let sections = controller.sections {
+            return sections.count
+        }
+
         return 0
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
     }
 
     func attemptFetch(){
@@ -46,6 +75,7 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         fetchRequest.sortDescriptors = [ dataSort ]
 
         let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        self.controller = controller
 
         do {
             try controller.performFetch()
@@ -82,6 +112,7 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
             if let indexPath = indexPath {
                 let cell = tableView.cellForRow(at: indexPath) as! ItemCell
                 // update the cell data
+                configureCell(cell: cell, indexPath: indexPath)
             }
             break
         case .move:
@@ -93,6 +124,26 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
             }
             break
         }
+    }
+
+    func generateTestData(){
+
+        let item = Item(context: context)
+        item.title = "iPhone X"
+        item.price = 999
+        item.details = "newest iphone that everyone want"
+
+        let item2 = Item(context: context)
+        item2.title = "MacBook Pro"
+        item2.price = 1800
+        item2.details = "want another macbook"
+
+        let item3 = Item(context: context)
+        item3.title = "Surface"
+        item3.price = 1800
+        item3.details = "never used it before, just wanna a try"
+
+        ad.saveContext()
     }
 
 }
--- DreamLister/View/ItemCell.swift
+++ DreamLister/View/ItemCell.swift
@@ -11,12 +11,15 @@ import UIKit
 class ItemCell: UITableViewCell {
 
     @IBOutlet weak var thumb: UIImageView!
-
     @IBOutlet weak var title: UILabel!
-
     @IBOutlet weak var price: UILabel!
-
     @IBOutlet weak var details: UILabel!
-    
+
+    func configureCell(item: Item){
+        title.text = item.title
+        price.text = "$\(item.price)"
+        details.text = item.details
+        
+    }
 
 }
